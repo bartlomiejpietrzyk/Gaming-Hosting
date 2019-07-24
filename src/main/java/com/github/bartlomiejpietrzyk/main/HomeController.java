0550ac4e04272372fd22a0bd6f9e2848eb06c658
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-07-25 00:20:48 +0200

    Spring security update

---
+++ src/main/java/com/github/bartlomiejpietrzyk/main/HomeController.java
@@ -0,0 +1,26 @@
+package com.github.bartlomiejpietrzyk.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+}
