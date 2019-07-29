66fc829017f974ed7432095c0c71db541fba11f4
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-07-29 13:32:13 +0200

    Server repo

---
+++ src/main/java/com/github/bartlomiejpietrzyk/server/ServerRepository.java
@@ -0,0 +1,23 @@
+package com.github.bartlomiejpietrzyk.server;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ServerRepository extends JpaRepository<Server, Long> {
+
+    @Override
+    Server getOne(Long aLong);
+
+    List<Server> findAll();
+
+    <S extends Server> S save(S s);
+
+    boolean exists(Long id);
+
+    long count();
+
+    void delete(Long id);
+}
