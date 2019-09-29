3c5f94b1b1b0aa7e75785e1d047d398850bfb37b
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-09-29 23:23:41 +0200

    access hanlder + public sites controller

--- src/main/java/pl/bartlomiejpietrzyk/config/SecurityConfig.java
+++ src/main/java/pl/bartlomiejpietrzyk/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.bartlomiejpietrzyk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import pl.bartlomiejpietrzyk.account.SpringDataUserDetailsService;
+import pl.bartlomiejpietrzyk.handler.CustomAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomAuthenticationSuccessHandler successHandler;
 
+    @Autowired
+    public SecurityConfig(CustomAuthenticationSuccessHandler successHandler) {
+        this.successHandler = successHandler;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -25,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .successHandler(successHandler)
                 .and()
                 .logout()
                 .invalidateHttpSession(true)
--- src/main/java/pl/bartlomiejpietrzyk/controller/HomeController.java
+++ src/main/java/pl/bartlomiejpietrzyk/controller/HomeController.java
@@ -1,11 +1,40 @@
 package pl.bartlomiejpietrzyk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    public String homePage() {
+    @GetMapping("/")
+    public String root() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "panel/dashboard";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/service")
+    public String service() {
+        return "service";
+    }
+
+    @GetMapping("/servers")
+    public String servers() {
+        return "servers";
+    }
+
 }
